5c51fad0dfa1ce29f199e552552ab6a154e21d4c
Akinwande Tomisin <tomjames156@gmail.com>
2022-06-20 11:10:37 -0600

    added effect for side navigation

--- movies.js
+++ movies.js
@@ -1,16 +1,23 @@
 const links = document.querySelectorAll("div.side-navigation > a")
 const sideNav = document.querySelector("div.side-navigation");
+const mainContent = document.querySelector("div.main-content")
 
 function closeMenu(){
     sideNav.style.width = 0;
+
     for(let link of links){
         link.style.display = "none";
     }
+
+    mainContent.style.opacity = "100%";
 }
 
 function openMenu(){
-    sideNav.style.width = "30%";
+    sideNav.style.width = "40%";
+    
     for(let link of links){
         link.style.display = "block";
     }
+
+    mainContent.style.opacity = "50%";
 }
