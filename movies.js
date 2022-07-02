a8ef464435cdd339ae00cf385fcff02980c7c93f
Akinwande Tomisin <tomjames156@gmail.com>
2022-07-02 05:28:12 +0100

    added ratings and genres feature

--- movies.js
+++ movies.js
@@ -1,9 +1,6 @@
 const links = document.querySelectorAll("div.side-navigation > a");
 const sideNav = document.querySelector("div.side-navigation");
 const mainContent = document.querySelector("div.main-content");
-const movieImages = document.querySelectorAll("img.movie-img");
-const aboutSpace = document.querySelectorAll("div.about-movie");
-const starSpace = document.getElementById("stars");
 
 function closeMenu(){
     sideNav.style.width = 0;
@@ -25,22 +22,14 @@ function openMenu(){
     mainContent.style.opacity = "50%";
 }
 
-function listItems(arr){
-    let output = "<ul>";
+function showGenres(arr){
+    let output = "<ul class='genres'>";
 
     for(i = 0; i < arr.length; i++){
         output += `<li>${arr[i]}</li>`;
     }
     
     return output += "</ul>";
-}
-
-function addGenres(genres){
-    for(let image of movieImages){
-        image.addEventListener("click", function(){
-            image.parentElement.children[1].innerHTML = showGenres(genres);
-        });
-    }
 }
 
 function showStars(stars){
@@ -51,7 +40,7 @@ function showStars(stars){
 
     for(let i = current_stars; i >= 0.5; i--){
         if(i == 0.5){
-            rating += `<i class="fa-solid fa-star-half"></i>`;
+            rating += `<i class="fa-solid fa-star-half-stroke"></i>`;
         }else{
             rating += `<i class="fa-solid fa-star"></i>`;
         }
@@ -64,51 +53,16 @@ function showStars(stars){
     return rating;
 }
 
-movieImages[0].click();
+function aboutMovie(receiver, stars, genres){
+    receiver.style.opacity = "70%";
+    receiver.parentElement.children[1].innerHTML = showStars(stars) + showGenres(genres);
+}
 
-function aboutMovie(){
-    let stars = 0;
-    let genres = [];
-    for(let i = 0; i < movieImages.length; i++){
-        movieImages[i].addEventListener("click", function(){
-            let val = i;
-            switch(val){
-                case 0:
-                    stars = 4.5;
-                    genres = ['Sci-Fi', 'Horror'];
-                    console.log(genres);
-                    movieImages[i].style.border = "solid 1px red";
-                    break;
-                case 1:
-                    stars = 4.5;
-                    genres = ['Horror', 'Murder Mystery'];
-                    movieImages[i].parentElement.children[1].innerHTML= listItems(genres);
-                    break;
-                case 2:
-                    stars = 3.5;
-                    genres = ['Sitcom'];
-                    break;
-                case 3:
-                    stars = 5;
-                    genres = ['Sci-Fi', 'Horror', 'Comedy', 'Action'];
-                    break;
-                case 4:
-                    stars = 4;
-                    genres = ['Murder Mystery'];
-                    break;
-                case 5:
-                    stars = 4;
-                    genres = ['Murder Mystery'];
-                    break;
-                case 6:
-                    stars = 4.5;
-                    genres = ['Sci-Fi', 'Comedy', 'Animation'];
-                    break;
-                case 7:
-                    stars = 5;
-                    genres = ['Murder Mystery', 'Comedy'];
-                    break;
-            }
-        })
-    }
+function retain(receiver, stars, genres){
+    receiver.parentElement.children[1].innerHTML = showStars(stars) + showGenres(genres);
+}
+
+function reset(receiver){
+    receiver.style.opacity = "100%";
+    receiver.parentElement.children[1].innerHTML = "";
 }
